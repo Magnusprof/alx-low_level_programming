88cea0af021521bc217d40bf313a8af40f59e880
Magnusprof <adekeyemoyin@gmail.com>
2023-06-15 11:49:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/** main - A program that print to standard error
+/**
+ * main - A program that print to standard error
  * Return : Always 1.
  */
 
-int main (void)
+int main(void)
 
 {
-	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
+}
