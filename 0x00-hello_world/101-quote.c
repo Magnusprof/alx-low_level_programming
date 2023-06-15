5ecd341cc29e5e6ebc5d3350fb1b7da5248a8f27
Magnusprof <adekeyemoyin@gmail.com>
2023-06-15 12:21:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A program that print to standard error
- * Return : Always 1.
+ * main - A program that prints  line to the standard error
+ * Return 1 (success)
  */
 
 int main(void)
 
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar,2015-10-19\n", stdout);
 	return (1);
 }
