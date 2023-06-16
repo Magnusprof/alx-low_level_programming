29168e94cde899876cb87c8ee85bb6851725362e
Magnusprof <adekeyemoyin@gmail.com>
2023-06-16 07:37:36 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - A program to prints all possible different
+ * combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int p, q;
+for (p = 48; p <= 56; p++)
+{
+for (q = 49; q <= 57; q++)
+{
+if (q > p)
+{
+putchar(p);
+putchar(q);
+if (p != 56 || q != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
