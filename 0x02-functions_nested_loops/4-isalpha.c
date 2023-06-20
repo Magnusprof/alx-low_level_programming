eac91fe3fbb36d7f81502b05371ea07c18de78ff
Magnusprof <adekeyemoyin@gmail.com>
2023-06-20 13:20:51 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - the program checks for alphabetic character
+ * _isalpha - the program checks for alphabetic character
  * @c: The character to  be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
