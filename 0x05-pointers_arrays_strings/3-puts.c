50667079cc8d1c1c7f6368cc0d1d9e105e88e7c0
Magnusprof <adekeyemoyin@gmail.com>
2023-06-26 08:08:08 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that prints a string,
+ * followed by a new line, to stdout
+ * @str: pointer to the string
+ * Return: string and new line
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
